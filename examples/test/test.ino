429a1b6857eb5d75a471011749407019ca5cec4a
GyverLibs <84599917+GyverLibs@users.noreply.github.com>
2022-02-01 20:24:27 +0300

    upd

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,13 +1,11 @@
-// управляем 4 пин вентилятором и измеряем обороты
-#define PWM_PIN 10    // ШИМ пин (подключать через резик 100-1000 Ом)
-#define TACH_PIN 2    // пин тахометра (желательна внешняя подтяжка 10к к VCC)
+// измеряем обороты вентилятора от пк
+#define TACH_PIN 2    // пин тахометра (желательна внешняя подтяжка 4.7к к VCC)
 
 #include <Tachometer.h>
 Tachometer tacho;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PWM_PIN, OUTPUT);
 
   // пин тахометра вентилятора подтягиваем к VCC
   pinMode(TACH_PIN, INPUT_PULLUP);
@@ -15,11 +13,8 @@ void setup() {
   // настраиваем прерывание
   attachInterrupt(0, isr, FALLING);
 
-  // включаем вентиль на скорость 150 (из 255)
-  analogWrite(PWM_PIN, 190);
-  
-  //tacho.setWindow(20);		// установка количества тиков для счёта времени (по умолч 10)
-  //tacho.setTimeout(2000);	// таймаут прерываний (мс), после которого считается что вращение прекратилось
+  //tacho.setWindow(20);    // установка количества тиков для счёта времени (по умолч 10)
+  //tacho.setTimeout(2000); // таймаут прерываний (мс), после которого считается что вращение прекратилось
 }
 
 // обработчик прерывания
@@ -28,12 +23,12 @@ void isr() {
 }
 
 void loop() {
-  // выводим несколько раз в секунду
+  // выводим 10 раз в секунду
   static uint32_t tmr;
   if (millis() - tmr > 100) {
     tmr = millis();
-    Serial.println(tacho.getRPM());			// RPM
-	//Serial.println(tacho.getHz());		// Hz
-	//Serial.println(tacho.getUs());	// us
+    Serial.println(tacho.getRPM());     // RPM
+    //Serial.println(tacho.getHz());    // Hz
+    //Serial.println(tacho.Us());       // us
   }
 }
