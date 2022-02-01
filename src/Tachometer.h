924cb4e91474a9e692469078b4746181f0f8aaa1
GyverLibs <84599917+GyverLibs@users.noreply.github.com>
2022-02-01 20:22:11 +0300

    upd

--- src/Tachometer.h
+++ src/Tachometer.h
@@ -7,7 +7,8 @@
     - Забирай getRPM() частоту в оборотах в минуту
     - Забирай getHz() частоту в Герцах
     - Забирай getPeriod() период в мкс
-    - Встроенный медианный фильтр
+    - Встроенный медианный фильтр от скачков
+    - Встроенный таймаут отсутствия сигнала
     
     AlexGyver, alex@alexgyver.ru
     https://alexgyver.ru/
@@ -16,76 +17,69 @@
     Версии:
     v1.1
     v1.2 - оптимизация
+    v1.3 - улучшена стабильность, уменьшен вес кода
 */
 
-#ifndef Tachometer_h
-#define Tachometer_h
+#ifndef _Tachometer_h
+#define _Tachometer_h
 #include <Arduino.h>
 
 class Tachometer {
 public:
     // тикер - вызывать в прерывании по фронту
-    void tick() {   // tachoTime - время в мкс каждых _window тиков
-        if (!ticks--) {
-            ticks = _window - 1;
-            tachoTime = micros() - tachoTimer;
-            tachoTimer += tachoTime;  //== tachoTimer = micros();
-            tachoTime = median3(tachoTime);
+    void tick() {
+        if (ticks <= window) {
+            if (!ticks) tmr = micros();
+            if (ticks == window) tmr = micros() - tmr;
+            ticks++;
         }
     }
     
     // установка количества тиков для счёта времени (по умолч 10)
-    void setWindow(int window) {
-        _window = window;
+    void setWindow(uint8_t nwindow) {
+        window = nwindow;
+        ticks = 0;
     }
     
     // таймаут прерываний (мс), после которого считается что вращение прекратилось (по умолч 1000)
-    void setTimeout(uint32_t tout) {
-        _tout = tout * 1000ul;
+    void setTimeout(uint16_t ntout) {
+        tout = ntout;
     }
     
     // получить обороты в минуту
-    uint16_t getRPM() {
-        if (micros() - tachoTimer > _tout || tachoTime == 0) return 0;
-        else return _window * 60000000ul / tachoTime;
+    uint32_t getRPM() {
+        return getTime() ? (60000000ul / prd) : 0;
     }
 
-    // получить герцы
+    // получить частоту в герцах
     float getHz() {
-        if (micros() - tachoTimer > _tout || tachoTime == 0) return 0;
-        else return _window * 1000000.0 / tachoTime;
+        return getTime() ? (1000000.0 / prd) : 0;
     }
     
     // получить период в мкс
     uint32_t getUs() {
-        if (micros() - tachoTimer > _tout || tachoTime == 0) return 0;
-        else return tachoTime / _window;
-    }
-
-    // получить период в мкс (legacy)
-    uint32_t getPeriod() {
-        return getUs();
+        return getTime() ? prd : 0;
     }
 
 private:
-    // быстрая медиана
-    uint32_t median3(uint32_t value) {
-        buf[counter] = value;
-        if (++counter > 2) counter = 0;
-        if ((buf[0] <= buf[1]) && (buf[0] <= buf[2])) return (buf[1] <= buf[2]) ? buf[1] : buf[2];
-        else {
-            if ((buf[1] <= buf[0]) && (buf[1] <= buf[2])) return (buf[0] <= buf[2]) ? buf[0] : buf[2];
-            else return (buf[0] <= buf[1]) ? buf[0] : buf[1];
+    bool getTime() {
+        if (ticks > window) {
+            tmrMs = millis();
+            buf[count] = (uint32_t)tmr / window;
+            if (++count >= 3) count = 0;
+            prd = (max(buf[0], buf[1]) == max(buf[1], buf[2])) ? max(buf[0], buf[2]) : max(buf[1], min(buf[0], buf[2]));
+            ticks = 0;
         }
+        return (millis() - tmrMs < tout && prd);
     }
-
-    volatile uint32_t tachoTime = 100000;       // для плавного старта значений
-    volatile uint32_t tachoTimer = micros();
-    volatile int ticks = 0;
-    volatile int _window = 10;
     
-    uint32_t _tout = 1000000;
-    uint32_t buf[3] = {100000, 100000, 100000}; // для плавного старта значений
-    uint8_t counter = 0;
+    volatile uint32_t tmr;
+    volatile uint8_t ticks = 0;
+    
+    uint8_t window = 10, count = 0;
+    uint32_t tmrMs = millis();
+    uint32_t prd = 0;
+    uint32_t buf[3];
+    uint16_t tout = 1000;
 };
 #endif
